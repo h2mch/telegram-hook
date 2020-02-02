c5a72c36ce659462dbb17c9496c46e3c193741af
Heinz Marti <github@h2m.ch>
2020-02-02 07:04:54 +0100

    add scripts for testing purpose

--- script.cmd
+++ script.cmd
@@ -3,7 +3,13 @@ C:\source\graalvm\telegram-hook
 # Build and deploy
 λ docker build -t h2mch/webhook .
 λ docker tag h2mch/webhook:latest gcr.io/quarkus-265809/webhook
-λ docker push gcr.io/quarkus-265809/
+λ docker push gcr.io/quarkus-265809/webhook
 
 #Test
 λ docker run --rm -it -p 8080:8080 h2mch/clouddeployment
+
+
+
+λ gcloud container images delete gcr.io/quarkus-265809/webhook --force-delete-tags
+
+gcloud container images list-tags gcr.io/quarkus-265809/webhook --filter='-tags:*'  --format='get(digest)' --limit=$BIG_NUMBER
