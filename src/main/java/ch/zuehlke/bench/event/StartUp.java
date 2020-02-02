266d3b0a4e44ce538ab0206d7d2ec9eb19eb4281
Heinz Marti <github@h2m.ch>
2020-02-02 07:03:37 +0100

    include applicationscoped.
    without direct reference, the beans are not reachable at runtime with a native compilation (aot?)

---
+++ src/main/java/ch/zuehlke/bench/event/StartUp.java
@@ -0,0 +1,40 @@
+package ch.zuehlke.bench.event;
+
+import org.jboss.logging.Logger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import ch.zuehlke.bench.skiarea.TitlisService;
+import ch.zuehlke.bench.transport.DelayService;
+import ch.zuehlke.bench.weather.WeatherService;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+
+@ApplicationScoped
+public class StartUp {
+
+    private static final Logger LOGGER = Logger.getLogger("StartUp");
+
+
+    @Inject
+    WeatherService weatherService;
+
+    @Inject
+    DelayService delayService;
+
+    @Inject
+    TitlisService ts;
+
+    void onStart(@Observes StartupEvent ev) {
+        LOGGER.info("The application is starting...");
+
+
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOGGER.info("The application is stopping...");
+    }
+
+}
