8ff74ab64d2a3110e8d5934220af6d3e0bb5d2e8
Heinz Marti <github@h2m.ch>
2020-02-18 21:29:14 +0100

    extract service url to property file

--- src/main/java/ch/zuehlke/bench/skiarea/TitlisService.java
+++ src/main/java/ch/zuehlke/bench/skiarea/TitlisService.java
@@ -1,5 +1,6 @@
 package ch.zuehlke.bench.skiarea;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -7,8 +8,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 import ch.zuehlke.bench.telegram.TelegramCommand;
 
@@ -17,12 +20,17 @@ public class TitlisService implements TelegramCommand {
 
 
     private static final Logger LOG = Logger.getLogger(TitlisService.class);
-    private static final String URL_DE = "https://www.titlis.ch/de/live/offene-Anlagen";
+
+    @Inject
+    @ConfigProperty(name = "skiarea.titlis.url")
+    Optional<String> url;
 
     private Connection connect;
 
     public TitlisService() {
-        connect = Jsoup.connect(URL_DE);
+        if (url != null && url.isPresent()) {
+            connect = Jsoup.connect(url.get());
+        }
     }
 
     TitlisService(Connection connect) {
@@ -41,11 +49,14 @@ public class TitlisService implements TelegramCommand {
 
 
     public String extractLiveLiftsInformation() throws IOException {
+        if (url.isEmpty()) {
+            return "url for titlis not configured";
+        }
         Document doc = connect.get();
         Elements callout_primary = doc.getElementsByClass("callout primary");
         if (callout_primary == null || callout_primary.size() != 1) {
             LOG.warnf("Command not find information on page. %s", doc.body());
-            throw new InformationNotFoundException("Could not extract Information from url '" + URL_DE + "'.");
+            throw new InformationNotFoundException("Could not extract Information from url '" + url + "'.");
         }
         String text = callout_primary.get(0).text();
         String lastUpdate = callout_primary.next().text();
