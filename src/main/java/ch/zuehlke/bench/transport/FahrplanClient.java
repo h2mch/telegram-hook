6eadae7ebddce510520026bf40ba2deb88b69058
Heinz Marti <github@h2m.ch>
2020-05-14 10:48:41 +0200

    revert to default names

--- src/main/java/ch/zuehlke/bench/transport/FahrplanClient.java
+++ src/main/java/ch/zuehlke/bench/transport/FahrplanClient.java
@@ -9,7 +9,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/bin")
-@RegisterRestClient(configKey = "fahrplan-client")
+@RegisterRestClient
 public interface FahrplanClient {
 
     @GET
